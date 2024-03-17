5801bfae306dec86a3fe64ba27d6f8e0828b1143
Adnan Soudki <asoudki@uwaterloo.ca>
2024-03-17 19:28:11 -0400

    Fix up integration of mpu_6050 with edf
    It turns out I missed a lot more than I thought. The practice of
    dividing sensor reads and calculating angles doesn't make sense in
    retrospect, given how the latter task has a trivial runtime and must
    always happen after the former. The use of queues also doesn't make
    sense when I can just protect a known single instance of the data with a
    mutex.
    
    Signed-off-by: Adnan Soudki <asoudki@uwaterloo.ca>

--- integration_demo.c
+++ integration_demo.c
@@ -5,114 +5,87 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
-#include <queue.h>
+#include <semphr.h>
 
 const double GYRO_WEIGHT = 0.8;
 const uint32_t SAMPLES_CALIBRATION = 10000;
 
+TaskHandle_t mpu_6050_task_handle = NULL;
+TaskHandle_t print_angles_task_handle = NULL;
+
+const TickType_t mpu_6050_task_period = pdMS_TO_TICKS(10);
+const TickType_t print_angles_task_period = pdMS_TO_TICKS(100);
+
 typedef struct {
-    vec_double_t accel_data;
-    vec_double_t gyro_data;
-    double dt;
-} mpu_6050_data_t;
+    mpu_6050_t * mpu_6050;
+    vec_double_t * angles;
+} mpu_6050_task_data_t;
 
-TaskHandle_t mpu_6050_read_task_handle = NULL;
-TaskHandle_t mpu_6050_process_task_handle = NULL;
-TaskHandle_t mpu_6050_print_task_handle = NULL;
+SemaphoreHandle_t angles_mutex;
 
-const TickType_t mpu_6050_read_task_period = pdMS_TO_TICKS(1);
-const TickType_t mpu_6050_process_task_period = pdMS_TO_TICKS(10);
-const TickType_t mpu_6050_print_task_period = pdMS_TO_TICKS(100);
+// Estimates the current roll, pitch and yaw of the MPU by fusing accel + gyro data
+void estimate_angles(vec_double_t accel_data, vec_double_t gyro_data, double dt, vec_double_t * angles) {
+    // estimate angles based on accel data
+    double accel_angle_x = (atan(accel_data.y / sqrt(pow(accel_data.x, 2) + pow(accel_data.z, 2))) * 180 / M_PI);
+    double accel_angle_y = (atan(-1 * accel_data.x / sqrt(pow(accel_data.y, 2) + pow(accel_data.z, 2))) * 180 / M_PI);
 
-QueueHandle_t mpu_6050_data_queue;
-QueueHandle_t angles_queue;
+    // estimate angles based on gyro data
+    double gyro_angle_x = angles->x + gyro_data.x * dt;
+    double gyro_angle_y = angles->y + gyro_data.y * dt;
+    double gyro_angle_z = angles->z + gyro_data.z * dt;
 
-void mpu_6050_read_task(void *pvParameters)
+    // fuse estimates with complementary filter to determine roll and pitch
+    angles->x = GYRO_WEIGHT*gyro_angle_x + (1-GYRO_WEIGHT)*accel_angle_x;
+    angles->y = GYRO_WEIGHT*gyro_angle_y + (1-GYRO_WEIGHT)*accel_angle_y;
+    
+    // yaw calculation is its own thing since there's nothing from accel to fuse with
+    angles->z = gyro_angle_z;
+}
+
+// Gets data from 
+void mpu_6050_task(void *pvParameters)
 {
-    mpu_6050_t* mpu_6050 = (mpu_6050_t*) pvParameters;
-    mpu_6050_data_t data;
+    mpu_6050_task_data_t * mpu_6050_task_data = (mpu_6050_task_data_t*) pvParameters;
 
-    while (1) {
-        // Read raw data, retry task if it fails
+    mpu_6050_t * mpu_6050 = mpu_6050_task_data->mpu_6050;
+    vec_double_t * angles = mpu_6050_task_data->angles;
+
+    while(1) {
         if(mpu_6050_read_raw(mpu_6050) != MPU_6050_RC_OK) {
             continue;
         }
-        // Convert read data, retry task if it fails
+
         if(mpu_6050_convert_read(mpu_6050) != MPU_6050_RC_OK) {
             continue;
         }
 
-        // Put relevant data for processing into mpu_6050_data struct
-        copy_double_vector(&mpu_6050->accel_data, &data.accel_data);
-        copy_double_vector(&mpu_6050->gyro_data, &data.gyro_data);
-        data.dt = mpu_6050->dt;
+        xSemaphoreTake(angles_mutex, portMAX_DELAY);
+        {
+            estimate_angles(mpu_6050->accel_data, mpu_6050->gyro_data, mpu_6050->dt, angles);
+        }
+        xSemaphoreGive(angles_mutex);
 
-        // Send mpu_6050_data struct to process task
-        xQueueSend(mpu_6050_data_queue, &data, portMAX_DELAY);
-
-        // Delay to control read frequency
-        vTaskDelay(pdMS_TO_TICKS(1));
+        edf_complete_task(mpu_6050_task_handle);
     }
 }
 
-void mpu_6050_process_task(void *pvParameters)
+void print_angles_task(void *pvParameters)
 {
-    mpu_6050_data_t data;
-    vec_double_t angles = {0, 0, 0};
+    vec_double_t * angles = (vec_double_t *)pvParameters;
+
+    vec_double_t current_angles = {0.0, 0.0, 0.0};
 
     while (1) {
-        // Receive latest data from mpu_6050_read_task
-        if( xQueueReceive(mpu_6050_data_queue, &data, portMAX_DELAY) == pdPASS) {
-            // get references to pieces of data (mostly for clarity)
-            vec_double_t * accel_data = &data.accel_data;
-            vec_double_t * gyro_data = &data.gyro_data;
-            double dt = data.dt;
-            
-            // estimate angles based on accel data
-            double accel_angle_x = (atan(accel_data->y / sqrt(pow(accel_data->x, 2) + pow(accel_data->z, 2))) * 180 / M_PI);
-            double accel_angle_y = (atan(-1 * accel_data->x / sqrt(pow(accel_data->y, 2) + pow(accel_data->z, 2))) * 180 / M_PI);
-
-            // estimate angles based on gyro data
-            double gyro_angle_x = angles.x + gyro_data->x * dt;
-            double gyro_angle_y = angles.y + gyro_data->y * dt;
-            double gyro_angle_z = angles.z + gyro_data->z * dt;
-
-            // fuse estimates with complementary filter to determine roll and pitch
-            angles.x = GYRO_WEIGHT*gyro_angle_x + (1-GYRO_WEIGHT)*accel_angle_x;
-            angles.y = GYRO_WEIGHT*gyro_angle_y + (1-GYRO_WEIGHT)*accel_angle_y;
-            
-            // yaw calculation is its own thing since there's nothing from accel to fuse with
-            angles.z = gyro_angle_z;
-
-            // Send angles to print task
-            xQueueSend(angles_queue, &angles, portMAX_DELAY);
-
-            // Delay to control frequency
-            vTaskDelay(pdMS_TO_TICKS(10));
+        xSemaphoreTake(angles_mutex, portMAX_DELAY);
+        {
+            copy_double_vector(angles, &current_angles);
         }
-    }
-}
+        xSemaphoreGive(angles_mutex);
 
-void mpu_6050_print_task(void *pvParameters)
-{
-    vec_double_t angles;
+        // Print current angles
+        printf("%f/%f/%f\n", current_angles.x, current_angles.y, current_angles.z);
 
-    while (1) {
-        // Initially assume the queue is empty
-        BaseType_t xReceived = pdFALSE;
-
-        // Go through the queue until the latest angle passed from mpu_6050_process_task is received
-        while(xQueueReceive(angles_queue, &angles, 0) == pdPASS) {
-            xReceived = pdTRUE;
-        }
-
-        // If at least one item was received, print the angles
-        if(xReceived == pdTRUE) {
-            printf("%f/%f/%f\n", angles.x, angles.y, angles.z);
-        }
-
-        // Delay to control frequency
-        vTaskDelay(pdMS_TO_TICKS(100));
+        edf_complete_task(print_angles_task_handle);
     }
 }
 
@@ -129,38 +102,48 @@ int main()
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     i2c_init(i2c_default, 100 * 1000);
 
+    // Sufficient delay to give user time to boot up a serial terminal
+    sleep_ms(5000);
+
     // Initialize and calibrate MPU-6050
     mpu_6050_t mpu_6050;
     mpu_6050_rc_t rc = mpu_6050_init(&mpu_6050, i2c_default);
+
     while(rc != MPU_6050_RC_OK)
     {
         // Repeatedly try to initialize until successful
         printf("rc = %d, Sensor ID = %x\n", rc, mpu_6050.sensor_id);
-        sleep_ms(1000);
         rc = mpu_6050_init(&mpu_6050, i2c_default);
     }
     mpu_6050_reset(&mpu_6050);
+
+    printf("starting calibration\n");
     mpu_6050_calibrate(&mpu_6050, SAMPLES_CALIBRATION);
+    printf("calibration done\n");
 
-    // Create queues
-    mpu_6050_data_queue = xQueueCreate(20, sizeof(mpu_6050_data_t));
-    angles_queue = xQueueCreate(20, sizeof(vec_double_t));
-
-    // Create tasks, passing the address of mpu_6050 as the parameter
-    xTaskCreate(mpu_6050_read_task, "MPU-6050 Read Task", configMINIMAL_STACK_SIZE, (void*)&mpu_6050, EDF_UNSELECTED_PRIORITY, &mpu_6050_read_task_handle);
-    xTaskCreate(mpu_6050_process_task, "MPU-6050 Process Task", configMINIMAL_STACK_SIZE, NULL, EDF_UNSELECTED_PRIORITY, &mpu_6050_process_task_handle);
-    xTaskCreate(mpu_6050_print_task, "MPU-6050 Print Task", configMINIMAL_STACK_SIZE, NULL, EDF_UNSELECTED_PRIORITY, &mpu_6050_print_task_handle);
- 
-    // Define initial tasklist for EDF scheduler
-    edf_task_t tasklist[3] = 
+    // Create other variables used for tasks
+    vec_double_t angles = {0.0, 0.0, 0.0};
+    mpu_6050_task_data_t mpu_6050_task_data = 
     {
-        {.task_handle=mpu_6050_read_task_handle, .task_deadline=mpu_6050_read_task_period, .task_period=mpu_6050_read_task_period, .task_state=EDF_TASK_READY},
-        {.task_handle=mpu_6050_process_task_handle, .task_deadline=mpu_6050_process_task_period, .task_period=mpu_6050_process_task_period, .task_state=EDF_TASK_READY},
-        {.task_handle=mpu_6050_print_task_handle, .task_deadline=mpu_6050_print_task_period, .task_period=mpu_6050_print_task_period, .task_state=EDF_TASK_READY},
+        .mpu_6050 = &mpu_6050,
+        .angles = &angles,
     };
 
-    // Start the EDF scheduler
-    edf_start(tasklist, 3);
+    angles_mutex = xSemaphoreCreateMutex();
+
+    // Create tasks, passing the arguments to use by reference
+    xTaskCreate(mpu_6050_task, "MPU-6050 Task", 256, (void*)&mpu_6050_task_data, EDF_UNSELECTED_PRIORITY, &mpu_6050_task_handle);
+    xTaskCreate(print_angles_task, "MPU-6050 Print Task", 256, (void*)&angles, EDF_UNSELECTED_PRIORITY, &print_angles_task_handle);
+
+    // Define initial tasklist for EDF scheduler
+    edf_task_t tasklist[2] = 
+    {
+        {.task_handle=mpu_6050_task_handle, .task_deadline=mpu_6050_task_period, .task_period=mpu_6050_task_period, .task_state=EDF_TASK_READY},
+        {.task_handle=print_angles_task_handle, .task_deadline=print_angles_task_period, .task_period=print_angles_task_period, .task_state=EDF_TASK_READY},
+    };
+
+   printf("starting scheduler\n");
+   edf_start(tasklist, 2);
 
     while(1);
 }
